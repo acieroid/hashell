0eb70af8f8383a707a86f509ccc5800170d7e370
Quentin Stievenart (acieroid) <quentin.stievenart@gmail.com>
2009-05-20 18:20:15 +0200

    Now can read commands from a file

--- Main.hs
+++ Main.hs
@@ -3,8 +3,17 @@ module Main where
 import Prompt
 import Command
 import Control.OldException
+import System
+import IO
 
 main = do 
-  cmd <- runPrompt  
-  handle (\e -> putStrLn  $ "hashell: erreur: " ++ (show e)) (runCommand (parseCommand cmd) >> return ())
-  main
+  args <- getArgs
+  let prompt = length args == 0
+  cmd <- content args
+  handle (\e -> putStrLn  $ "hashell: erreur: " ++ 
+          (show e)) (runCommand (parseCommand cmd) >> return ())
+  if prompt then main else return ()
+  where content args | (length args) == 0 = runPrompt
+                     | otherwise = openFile (args !! 0) 
+                                     ReadMode >>= hGetContents
+      
